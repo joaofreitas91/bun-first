ea767f97155c9dcbeb36cfdcf4c0863c687c33c2
Joao Freitas <jp.contatoprofissional@gmail.com>
2024-04-14 20:16:43 -0300

    seed

---
+++ src/db/seed.ts
@@ -0,0 +1,70 @@
+import { faker } from '@faker-js/faker'
+import { db } from './connection'
+import { users, restaurants } from './schema'
+import chalk from 'chalk'
+import { eq } from 'drizzle-orm'
+
+/**
+ * On create a seed:
+ * Reset BD
+ * Create data
+ */
+
+/**
+ * Reset DB
+ */
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.yellow('✔️ Reseted DB'))
+
+/**
+ * Create users
+ */
+await db.insert(users).values([
+  {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    role: 'customer'
+  },
+  {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    role: 'customer'
+  }
+])
+
+console.log(chalk.yellow('✔️ Created users'))
+
+/**
+ * Create mananger
+ */
+const [mananger] = await db.insert(users).values([
+  {
+    name: faker.person.firstName(),
+    email: 'admin@admin.com',
+    role: 'mananger'
+  }
+]).returning({
+  id: users.id
+})
+
+console.log(chalk.yellow('✔️ Created mananger'))
+
+/**
+ * Create restaurant
+ */
+
+
+await db.insert(restaurants).values([
+  {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    manangerId: mananger.id
+  }
+])
+
+console.log(chalk.yellow('✔️ Created restaurant'))
+
+console.log(chalk.greenBright('Database seeded successfuly!'))
+process.exit()
