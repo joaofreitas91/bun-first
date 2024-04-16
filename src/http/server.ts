163ca1b25e127d9a6028ab9ae5fcb7a6baa0a2d7
Joao Freitas <jp.contatoprofissional@gmail.com>
2024-04-16 17:41:07 -0300

    feat: create restaurant with input validade data

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,48 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { users, restaurants } from '../db/schema'
+import { db } from '../db/connection'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World'
-})
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const {
+      restaurantName,
+      restaurantDescription,
+      manangerName,
+      manangerPhone,
+      manangerEmail,
+    } = body
+
+    const [manangerId] = await db
+      .insert(users)
+      .values({
+        name: manangerName,
+        phone: manangerPhone,
+        email: manangerEmail,
+        role: 'mananger',
+      })
+      .returning({
+        id: users.id,
+      })
+
+    await db.insert(restaurants).values({
+      manangerId: manangerId.id,
+      name: restaurantName,
+      description: restaurantDescription,
+    })
+
+    set.status = 204
+  },
+  {
+    body: t.Object({
+      restaurantName: t.String(),
+      restaurantDescription: t.String(),
+      manangerName: t.String(),
+      manangerPhone: t.String(),
+      manangerEmail: t.String(),
+    }),
+  },
+)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP Server Running with BUN')
