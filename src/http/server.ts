080c25314d3a96fa0a39e5930472ca6c1c580bc7
Joao Freitas <jp.contatoprofissional@gmail.com>
2024-04-20 18:39:16 -0300

    setup  JWT authenticate

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,26 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import jwt from '@elysiajs/jwt'
+import cookie from '@elysiajs/cookie'
+
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+
 import { env } from '../env'
 
-const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantID: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+
 const port = env.PORT || 3333
 
 app.listen(port, () => {
